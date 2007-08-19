b0b259e6a70921893004120c68cc934e31dc335c
Gábor Hojtsy <gabor@hojtsy.hu>
2007-08-19 16:57:02 +0000

    Another major update (and possibly the last before SoC is over, but this module will live on for sure :):
     - replaced unknown images with images from known source
       (known to be GPL), also from the same icon set
     - reorganized them to go to 'images', not 'img'
    
     - redone display of already translated strings, now the
       translator needs to proactively ask for the textarea
       for these to provide a new translation
    
     - made it possible to copy the previous translation to the
       editing field too
    
     - completely reworked the JavaScript to work as an add-on,
       now there are no buttons displayed, if there is no JS
       support in the browser, everything is added discretely
       (this makes it possible for mobile fanatics to translate
       from stripped down browsers, and such crazy stuff)
    
     - added an AHAH mechanism to retrieve detailed information
       on a string, which both saves us some performance, so we
       don't need to collect all the information on the initial
       page, but only on translator request, and it also helps
       declutter the interface.
    
    Everything seems to be working fine.

---
+++ images/README.txt
@@ -0,0 +1,4 @@
+$Id$
+
+These images are from the Nuove XT 2.2 GTK theme, which is licensed
+under the GNU GPL, the same license these modules are licensed under.
--- l10n_community.js
+++ l10n_community.js
@@ -1,19 +1,53 @@
 // $Id$
 
-function l10n_community_init() {
+l10nCommunity = {};
+
+/**
+ * Initialize action images: toolboxes and string copy buttons.
+ */
+l10nCommunity.init = function() {
+  // Hide textareas for strings where we already have translations.
+  $('#l10n-community-translate-form .hidden').css('display', 'none'); 
+
+  // Add information pane placeholder.
+  $('#l10n-community-translate-form fieldset').append('<div class="info-pane"></div>');
+
   // When the copy button is clicked, copy the original string value to the
   // translation field for the given strings. Relations are maintained with
   // the strings ideitifiers.
-  $('img.l10n-community-copy').click(function() {
+  $('span.l10n-community-copy').append('<img src="/l10n_server/sites/all/modules/l10n_server/images/edit.png" class="copy" alt="" />');
+  $('span.l10n-community-copy').click(function() {
     var id = $(this).attr('id').replace('l10n-community-copy-', '');
-    $('#l10n-commumnity-translation-' + id).val(Drupal.settings.l10n_strings[id]);
+    $('#l10n-commumnity-new-translation-' + id.replace('-t', '')).val(Drupal.settings.l10n_strings[id]);
   ;})
-  // Link-like behaviour with pointer mouse icon.
-  $('.form-item img').mouseover(function() {
-    $(this).css('cursor', 'pointer');
-  ;})
+
+  $('#l10n-community-translate-form .toolbox').each(function(){
+    // Add expand button to the toolbox.
+    $(this).append($(document.createElement('IMG')).attr('src', '/l10n_server/sites/all/modules/l10n_server/images/expand.png').attr('class', 'expand').click(function() {
+      var id = $(this).parent().attr('id').replace('l10n-community-toolbox-', '');
+      // Reveal textareas where the translation is done (if those were hidden).
+      $('.l10n-commumnity-wrapper-' + id).css('display', 'block');
+    ;}));
+    // Add a lookup button to invoke server side callback.
+    $(this).append($(document.createElement('IMG')).attr('src', '/l10n_server/sites/all/modules/l10n_server/images/lookup.png').attr('class', 'lookup').click(function() {
+      var uri = $(this).parent().parent().children('.l10n-community-sid-callback').attr('value');
+      // Ajax GET request to retrieve more details about this string.
+      $.ajax({
+        type: "GET",
+        url: uri,
+        success: function (data) {
+          var parts = data.split("\n\n");
+          $('#l10n-community-fields-' + parts[0] + ' .info-pane').empty().append(parts[1]);
+        },
+        error: function (xmlhttp) {
+          alert('An HTTP error '+ xmlhttp.status +' occured.\n'+ uri);
+        }
+      });
+    ;}));
+  });
 }
 
-$(function() {
-  l10n_community_init();
-});
+// Global killswitch
+if (Drupal.jsEnabled) {
+  $(document).ready(l10nCommunity.init);
+}
