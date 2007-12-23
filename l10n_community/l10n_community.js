9a58578ae1bd74109d665dad669a1f7aa09e9a50
Gábor Hojtsy <gabor@hojtsy.hu>
2007-12-23 22:36:13 +0000

    Another batch of hopefully exciting but probably debated updates:  - Down with the icons, hah. There are nice Unicode dingbats we can use, and we can color then however we want, even dynamically, as their role changes.  - Fixed suggestion copying, which did not work due to an index check problem.  - Implemented the suggestion decline feature, so suggestions can be declined one by one.  - Got rid of the 'Expand' function as it was just a superfluos UI element, as the edit buttons are much more to the point on the original string, the suggestions and translations.

--- l10n_community/l10n_community.js
+++ l10n_community/l10n_community.js
@@ -17,7 +17,7 @@ l10nCommunity.init = function() {
   // When the copy button is clicked, copy the original string value to the
   // translation field for the given strings. Relations are maintained with
   // the string ideitifiers.
-  $('span.l10n-community-copy').append('<img src="' + imagePath + 'edit.png" class="action" alt="" />');
+  //$('span.l10n-community-copy').append('<img src="' + imagePath + 'edit.png" class="action" alt="" />');
   $('span.l10n-community-copy').click(function() {
     var id = $(this).attr('id').replace('l10n-community-copy-', '');
     $('#l10n-community-translation-' + id.replace('-t', '')).val(Drupal.settings.l10n_strings[id]);
@@ -26,15 +26,8 @@ l10nCommunity.init = function() {
 
   $('#l10n-community-translate-form .toolbox').each(function(){
     
-    // Add expand button to the toolbox.
-    $(this).append($(document.createElement('IMG')).attr('src', imagePath + 'expand.png').attr('class', 'expand').attr('title', Drupal.settings.l10n_expand_help).click(function() {
-      var id = $(this).parent().parent().attr('id').replace('l10n-community-editor-', '');
-      // Reveal textareas where the translation is done (if those were hidden).
-      $('#l10n-community-wrapper-' + id).css('display', 'block');
-    ;}));
-    
     // Add a lookup button to invoke server side callback.
-    $(this).append($(document.createElement('IMG')).attr('src', imagePath + 'lookup.png').attr('class', 'lookup').attr('title', Drupal.settings.l10n_lookup_help).click(function() {
+    $(this).append(l10nCommunity.formatButton('&#x2600;', Drupal.settings.l10n_lookup_help, 'lookup', function() {
       var sid = $(this).parent().parent().attr('id').replace('l10n-community-editor-', '');
       // Ajax GET request to retrieve more details about this string.
       $.ajax({
@@ -43,6 +36,10 @@ l10nCommunity.init = function() {
         success: function (data) {
           // First empty, then load the data we get into the info pane.
           $('#l10n-community-editor-' + sid + ' .info-pane').css('display', 'block').empty().append(data);
+          $('#l10n-community-editor-' + sid + ' .lookup').addClass('disabled');
+          // Hide the has-suggestion marker if we don't have suggestions anymore.
+          // Could happen if we declined the last suggestion and reloading.
+          $('#l10n-community-editor-' + sid + ' .l10n-no-suggestions').parent().parent().find('.l10n-has-suggestion').hide();
         },
         error: function (xmlhttp) {
           // Being an internal/system error, this is not translatable.
@@ -50,7 +47,27 @@ l10nCommunity.init = function() {
         }
       });
     ;}));
+    
+    /*/ Add expand button to the toolbox.
+    $(this).append(l10nCommunity.formatButton(Drupal.settings.l10n_expand_help, 'expand', function() {
+      var id = $(this).parent().parent().attr('id').replace('l10n-community-editor-', '');
+      // Reveal textareas where the translation is done (if those were hidden).
+      $('#l10n-community-wrapper-' + id).css('display', 'block');
+      $('#l10n-community-editor-' + id + ' .expand').addClass('disabled');
+    }));*/
+    
   });
+}
+
+/**
+ * Add button formatted with the given data.
+ */
+l10nCommunity.formatButton = function(text, title, className, clickFunction) {
+  return $(document.createElement('SPAN')).attr('class', className + ' l10n-button').attr('title', title).
+         append($(document.createElement('B')).
+         append($(document.createElement('B')).
+         append($(document.createElement('B')).
+         append(text)))).click(clickFunction);
 }
 
 /**
@@ -84,10 +101,36 @@ l10nCommunity.approveSuggestion = function(tid, sid) {
 }
 
 /**
+ * Suggestion decline callback.
+ */
+l10nCommunity.declineSuggestion = function(tid, sid) {
+  // Invoke server side callback to save the decline action.
+  $.ajax({
+    type: "GET",
+    url: Drupal.settings.l10n_decline_callback + tid,
+    success: function (data) {
+      if (data == 'done') {
+        // Reload info pane.
+        $('#l10n-community-editor-'+ sid +' .lookup').click();
+      }
+      else {
+        alert(Drupal.settings.l10n_decline_error);
+      };
+    },
+    error: function (xmlhttp) {
+      // Being an internal/system error, this is not translatable.
+      alert('An HTTP error '+ xmlhttp.status +' occured.\n'+ uri);
+    }
+  });
+  // Return false for onclick handling.
+  return false;
+}
+
+/**
  * Suggestion editing copy callback.
  */
 l10nCommunity.copySuggestion = function(sid, translation) {
-  if (translation.indexOf("\O")) {
+  if (translation.indexOf("\O") > 0) {
     // If we have the null byte, suggestion has plurals, so we need to
     // copy over the distinct strings to the distinct textareas.
     var strings = translation.split("\O");
