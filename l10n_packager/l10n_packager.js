b75f8adb1bc9417519874efc230f9abfb681bc23
Gábor Hojtsy <gabor@hojtsy.hu>
2010-11-11 13:30:54 +0000

    #967704 followup by myself: behaviorify js code in download page

--- l10n_packager/l10n_packager.js
+++ l10n_packager/l10n_packager.js
@@ -2,11 +2,13 @@
 
 (function($) {
 
-  $('tr.l10n-packager-summary a.expand').click(function() {
-    var uri = $(this).parents('tr.l10n-packager-summary').attr('id').replace('l10n-packager-summary-', '');
-    $(this).parents('tr.l10n-packager-summary').fadeOut();
-    $('tr.l10n-packager-detail-' + uri).fadeIn();
-    return false;
-  });
-
+  Drupal.behaviors.l10nPackagerExpand = function(context) {
+    $('tr.l10n-packager-summary a.expand:not(.l10n-packager-processed)', context).addClass('l10n-packager-processed').click(function() {
+      var uri = $(this).parents('tr.l10n-packager-summary').attr('id').replace('l10n-packager-summary-', '');
+      $(this).parents('tr.l10n-packager-summary').fadeOut();
+      $('tr.l10n-packager-detail-' + uri).fadeIn();
+      return false;
+    });
+  }
+ 
 })(jQuery);
