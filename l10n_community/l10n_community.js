201b944aebd3090aa3a7032b46be1253b29c4e49
Sebastien Corbin <seb.corbin@gmail.com>
2013-09-29 18:53:18 +0200

    Fixed a JS error when l10nServerURLs would contain slashes

--- l10n_community/l10n_community.js
+++ l10n_community/l10n_community.js
@@ -7,7 +7,7 @@
         for (var url in Drupal.settings.l10nServerURLs) {
           // Look for links with this exact URL and replace with extended version.
           // This ensures we keep filter values while switching tabs.
-          $('a[href=' + url +']', context).not('.filter-exclude').attr('href', Drupal.settings.l10nServerURLs[url]);
+          $('a[href="' + url +'"]', context).not('.filter-exclude').attr('href', Drupal.settings.l10nServerURLs[url]);
         }
       }
     }
