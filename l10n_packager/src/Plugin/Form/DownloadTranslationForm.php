29e1c78fe7bd917f914a1f8a0651d42d62b61194
Mupsi <marine.gandy@posteo.net>
2022-04-08 10:49:22 +0200

    Started port of l10n_packager

---
+++ l10n_packager/src/Plugin/Form/DownloadTranslationForm.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Drupal\Core\Form\FormBase;
+
+final class DownloadTranslationForm extends FormBase {
+
+  public function getFormId() {
+    // TODO: Implement getFormId() method.
+  }
+
+  public function buildForm(
+    array $form,
+    \Drupal\Core\Form\FormStateInterface $form_state
+  ) {
+    // TODO: Implement buildForm() method.
+  }
+
+  public function submitForm(
+    array &$form,
+    \Drupal\Core\Form\FormStateInterface $form_state
+  ) {
+    // TODO: Implement submitForm() method.
+  }
+
+}
