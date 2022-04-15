0fb55125f50f3133f9ff6ab90d1ed18a30fea970
DDEV-Local User <nobody@example.com>
2022-04-15 09:11:26 +0000

    Added settings form for l10n_packager submodule.

--- l10n_packager/src/Form/SettingsForm.php
+++ l10n_packager/src/Form/SettingsForm.php
@@ -33,16 +33,16 @@ final class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state): array {
 
     $settings = $this->config('l10n_server.settings');
+    $manager = \Drupal::service('l10n_packager.manager');
 
-    $cron = $settings->get('l10n_packager_cron') ?? 0;
+    $cron = $settings->get('l10n_packager_cron') ?? L10N_PACKAGER_CRON;
     $form['l10n_packager_cron'] = array(
       '#title' => t('Generate packages on every Drupal cron run'),
       '#type' => 'checkbox',
       '#default_value' => $cron,
     );
 
-    //@todo: port l10n_packager_directory().
-    $directory = $settings->get('l10n_packager_directory') ?? null /*l10n_packager_directory()*/;
+    $directory = $settings->get('l10n_packager_directory') ?? $manager->directory();
     $form['l10n_packager_directory'] = array(
       '#title' => t('Directory for generated packages'),
       '#description' => t('The directory on the local file system to use to store packages generated. Either relative to the Drupal installation directory or an absolute path on your file system. Drupal should have read and write access to the files and directories found there.'),
@@ -54,8 +54,7 @@ final class SettingsForm extends ConfigFormBase {
       /*'#after_build' => array('l10n_packager_admin_check_directory'),*/
     );
 
-    //@todo: port l10n_packager_directory().
-    $update_url = $settings->get('l10n_packager_update_url') ?? null /*file_create_url(l10n_packager_directory())*/;
+    $update_url = $settings->get('l10n_packager_update_url') ?? L10N_PACKAGER_UPDATE_URL;
     $form['l10n_packager_update_url'] = array(
       '#title' => t('Root URL for translation downloads'),
       '#type' => 'textfield',
@@ -73,7 +72,7 @@ final class SettingsForm extends ConfigFormBase {
     );
 
     $options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    $release_limit = $settings->get('l10n_packager_release_limit') ?? L10N_PACKAGER_RELEASE_LIMIT_DEFAULT;
+    $release_limit = $settings->get('l10n_packager_release_limit') ?? L10N_PACKAGER_RELEASE_LIMIT;
     $form['l10n_packager_release_limit'] = array(
       '#title' => t('Number of releases to check at once'),
       '#description' => t('The number of releases to check on a manual or cron run.'),
@@ -83,7 +82,7 @@ final class SettingsForm extends ConfigFormBase {
     );
 
     $options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    $file_limit = $settings->get('l10n_packager_file_limit') ?? 1;
+    $file_limit = $settings->get('l10n_packager_file_limit') ?? L10N_PACKAGER_FILE_LIMIT;
     $form['l10n_packager_file_limit'] = array(
       '#title' => t('Maximum number of files to package at once'),
       '#description' => t('The number of files to package on a manual or cron run.'),
@@ -93,7 +92,7 @@ final class SettingsForm extends ConfigFormBase {
     );
 
     // Logging settings
-    $update = $settings->get('l10n_packager_file_limit') ?? 0;
+    $update = $settings->get('l10n_packager_update') ?? L10N_PACKAGER_UPDATE;
     $options = [3600, 10800, 21600, 32400, 43200, 86400, 172800, 259200, 604800, 1209600, 2419200, 4838400, 9676800];
     $callback = function ($value) {
       return \Drupal::service('date.formatter')->formatInterval($value);
@@ -114,13 +113,27 @@ final class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $origin = $form_state->getValue('connectors', []);
-    $enabled = array_values(array_filter($origin));
-    $disabled = array_keys(array_diff($origin, array_filter($origin)));
-    $this->connectorManager->removePluginConfigurationMultiple($disabled);
+    $manager = \Drupal::service('l10n_packager.manager');
+
+    $cron = $form_state->getValue('l10n_packager_cron', L10N_PACKAGER_CRON);
+    $directory = $form_state->getValue('l10n_packager_directory', $manager->directory());
+    $update_url = $form_state->getValue('l10n_packager_update_url', L10N_PACKAGER_UPDATE_URL);
+    $file_path = $form_state->getValue('l10n_packager_filepath', L10N_PACKAGER_FILEPATH);
+    $release_limit = $form_state->getValue('l10n_packager_release_limit', L10N_PACKAGER_RELEASE_LIMIT);
+    $file_limit = $form_state->getValue('l10n_packager_file_limit', L10N_PACKAGER_FILE_LIMIT);
+    $update = $form_state->getValue('l10n_packager_update', L10N_PACKAGER_UPDATE);
+
     $this->config('l10n_packager.settings')
-      ->set('enabled_connectors', $enabled)
+      ->set('l10n_packager_cron', $cron)
+      ->set('l10n_packager_directory', $directory)
+      ->set('l10n_packager_update_url', $update_url)
+      ->set('l10n_packager_filepath', $file_path)
+      ->set('l10n_packager_release_limit', $release_limit)
+      ->set('l10n_packager_file_limit', $file_limit)
+      ->set('l10n_packager_update', $update)
       ->save();
+
+    parent::submitForm($form, $form_state);
   }
 
 }
