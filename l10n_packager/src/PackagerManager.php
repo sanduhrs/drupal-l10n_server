e3818f611c7c77e8d77a4165df99d45fa6b2932a
sanduhrs <stefan@auditor.email>
2022-05-16 22:00:16 +0200

    Update packager manager

--- l10n_packager/src/PackagerManager.php
+++ l10n_packager/src/PackagerManager.php
@@ -6,52 +6,67 @@ namespace Drupal\l10n_packager;
 
 use Drush\Drush;
 
+/**
+ * Packager manager class.
+ */
 class PackagerManager {
 
   /**
    * Check releases that need repackaging.
    */
   public function checkUpdates() {
+    $config = \Drupal::config('l10n_server.settings');
+
     $count_check = $count_files = $time = 0;
-    $updates = array();
+    $updates = [];
 
-    $settings = \Drupal::config('l10n_server.settings');
-    $interval = $settings->get('l10n_packager_update') ?? 0;
+    if ($interval = $config->get('update')) {
+      $time_pre = microtime(true);
 
-    Drush::output()->writeln(dt('$interval: ' . $interval));
-
-    if ($interval) {
-
-      timer_start('l10n_packager');
       module_load_include('inc', 'l10n_packager');
-      $timestamp = REQUEST_TIME - $interval;
-      $file_limit = variable_get('l10n_packager_file_limit', 1);
-      $count_files = $count_check = 0;
+      $timestamp = \Drupal::time()->getRequestTime() - $interval;
+      $file_limit = $config->get('file_limit');
 
-      // Go for it: check releases for repackaging. We need project_uri for later.
-      $query = "SELECT r.rid, r.pid, r.title, pr.checked, pr.updated, pr.status, p.uri
-              FROM {l10n_server_release} r
-              INNER JOIN {l10n_server_project} p ON r.pid = p.pid
-              LEFT JOIN {l10n_packager_release} pr ON pr.rid = r.rid
-              WHERE pr.status IS NULL
-                 OR (pr.status = :status AND (pr.checked < :checked OR pr.updated < :updated))
-              ORDER BY pr.checked";
-      $result = db_query_range($query, 0, variable_get('l10n_packager_release_limit', 10),
-        array(':status' => L10N_PACKAGER_ACTIVE, ':checked' => $timestamp, ':updated' => $timestamp));
-      while ((!$file_limit || $file_limit > $count_files) && ($release = $result->fetchObject())) {
+      $query = \Drupal::database()
+        ->select('l10n_server_release', 'r');
+      $query
+        ->innerJoin('l10n_server_project', 'p');
+      $query
+        ->leftJoin('l10n_packager_release', 'pr');
+      $orGroup1 = $query->orConditionGroup()
+        ->condition('pr.checked', $timestamp, '<')
+        ->condition('pr.updated', $timestamp, '<');
+      $andGroup = $query->andConditionGroup()
+        ->condition('pr.status', 1)
+        ->condition($orGroup1);
+      $orGroup2 = $query->orConditionGroup()
+        ->condition('pr.status', NULL, 'IS NULL')
+        ->condition($andGroup);
+      $query->condition($orGroup1);
+      $query->condition($andGroup);
+      $query->condition($orGroup2);
+      $result = $query->range(0, $config->get('release_limit'))
+        ->execute();
+
+      while ((!$file_limit || $file_limit > $count_files)
+          && ($release = $result->fetchObject())) {
         // Set the release branch
         l10n_packager_release_set_branch($release);
         $updates = l10n_packager_release_check($release, FALSE, $file_limit - $count_files, NULL, TRUE);
         $count_files += count($updates);
         $count_check++;
       }
-      $timer = timer_stop('l10n_packager');
-      $time = $timer['time'];
+      $time_post = microtime(true);
+      $time = $time_post - $time_pre;
 
-      watchdog('l10n_packager', '@ms ms for %checked releases/%repack files.', array('%checked' => $count_check, '%repack' => $count_files, '@ms' => $time));
+      \Drupal::logger('l10n_packager')
+        ->notice('@ms ms for @checked releases/@repack files.', [
+          '@checked' => $count_check,
+          '@repack' => $count_files,
+          '@ms' => $time,
+        ]);
     }
-
-    return array($count_check, $count_files, $time);
+    return [$count_check, $count_files, $time];
   }
 
   /**
