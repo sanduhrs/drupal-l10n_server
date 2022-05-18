70271db4d1e2b51b7650f6a743774ad6f7f7409f
sanduhrs <stefan@auditor.email>
2022-05-18 08:47:04 +0200

    Fix translation download form downloads list

--- l10n_packager/src/Form/DownloadTranslationForm.php
+++ l10n_packager/src/Form/DownloadTranslationForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\l10n_packager\Form;
 
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\l10n_server\Entity\Project;
@@ -23,9 +24,24 @@ final class DownloadTranslationForm extends FormBase {
   }
 
   /**
+   * Get default project.
+   *
+   * @return \Drupal\l10n_server\Entity\Project
+   */
+  private static function getDefaultProject(): Project {
+    $query = \Drupal::entityQuery('l10n_server_project')
+      ->condition('title', static::DEFAULT_PROJECT);
+    $pids = $query->execute();
+    return Project::load(reset($pids));
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state, Project $project = NULL): array {
+    if (empty($project)) {
+      $project = DownloadTranslationForm::getDefaultProject();
+    }
     $form['project'] = [
       '#title' => t('Pick a project'),
       '#type' => 'textfield',
@@ -57,37 +73,96 @@ final class DownloadTranslationForm extends FormBase {
       '#value' => $this->t('Show downloads'),
       '#button_type' => 'primary',
     ];
+
+    $form += $this->l10n_packager_show_downloads($project);
     return $form;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state): void {
-    parent::validateForm($form, $form_state);
-    $defaultValues = $form_state->cleanValues()->getValues();
-
-    if (empty($defaultValues['project'])) {
-      $form_state->setErrorByName('project', $this->t('Invalid project name.'));
-    }
-
-    // @todo: port these methods to D9.
-    //$project = l10n_server_get_projects(
-    //  ['uri' => l10n_community_project_uri_by_title($defaultValues['values']['project'])]
-    //);
-    //if ($project) {
-    //  $form_state['values']['uri'] = $project->uri;
-    //}
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $form_state->setRedirect('l10n_packager.download_project_translations', [
+      'project' => $form_state->getValue('project') ?? self::DEFAULT_PROJECT,
+    ]);
   }
 
   /**
-   * {@inheritdoc}
+   * Generate list of file downloads for given project.
    */
-  public function submitForm(array &$form, FormStateInterface $form_state): void {
-    $form_state->setRedirect(
-      'l10n_packager.download_project_translations',
-      ['project' => $form_state->getValue('project') ?? self::DEFAULT_PROJECT]
-    );
+  private function l10n_packager_show_downloads($project) {
+    $date_formatter = \Drupal::service('date.formatter');
+    $files = $branches = [];
+
+    $query = \Drupal::database()->select('l10n_server_release', 'r');
+    $query->innerJoin('l10n_packager_release', 'pr', 'r.rid = pr.rid');
+    $query->innerJoin('l10n_packager_file', 'lf', 'r.rid = lf.rid');
+    $query->innerJoin('file_managed', 'f', 'lf.fid = f.fid');
+    $query->fields('r');
+    $query->fields('pr');
+    $query->addField('pr', 'checked', 'release_checked');
+    $query->condition('r.pid', $project->id());
+    $result = $query->execute();
+
+    foreach ($result as $item) {
+      // Trim versions to only the major version, and any preceeding components.
+      // For example, 1.2.3 → 1.x, 8.x-1.0-beta1 → 8.x-1.x.
+      $branch = preg_replace('/(\.\d+)?\.[^.]+$/', '', $item->title) . '.x';
+      $branches[$branch] = TRUE;
+      $files[$item->language][$branch][$item->rid] = $item;
+    }
+
+    if (empty($branches)) {
+      $form['empty'] = [
+        '#markup' => '<p>' . t('No translation downloads found for <em>%project</em>.', ['%project' => $project->label()]) . '</p>',
+        '#weight' => 100,
+      ];
+      return $form;
+    }
+
+    ksort($branches);
+    $languages = \Drupal::languageManager()->getLanguages();
+
+    $table = [];
+    foreach ($languages as $langcode => $language) {
+      $row = [];
+      // Start off the row with the language name and code.
+      $row[] = [
+        'data' => new FormattableMarkup('<a href=":link">@language_name</a>', [
+          '@language_name' => $language->getName(),
+          ':link' => '/translate/languages/' . $langcode,
+        ]
+      )];
+      foreach (array_keys($branches) as $branch) {
+        // Generate a cell for each major version.
+        if (!empty($files[$langcode][$branch])) {
+          krsort($files[$langcode][$branch]);
+          $latest_item = array_shift($files[$langcode][$branch]);
+          // @todo Fix l().
+          $cell = '<p>' . l($latest_item->title . ' (' . format_size($latest_item->filesize) . ')'/*, l10n_packager_get_download_url($project, $branch, $latest_item)*/) . '</p>';
+          $cell .= '<p class="l10n-packager-meta">' . t('Generated: @generated', ['@generated' => $date_formatter->format($latest_item->timestamp, 'custom', 'Y-M-d H:i')]) . '</p>';
+          $up_to_date = max($latest_item->checked, $latest_item->release_checked);
+          if ($up_to_date > $latest_item->timestamp) {
+            $cell .= '<p class="l10n-packager-meta">' . t('Up to date as of: @checked', ['@checked' => $date_formatter->format($up_to_date, 'custom', 'Y-M-d H:i')]) . '</p>';
+          }
+          $row[] = $cell;
+          $row[] = new FormattableMarkup($cell, []);
+        }
+        else {
+          $row[] = t('n/a');
+        }
+      }
+      $table[] = $row;
+    }
+
+    $header = array_merge([t('Languages')], array_keys($branches));
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $table,
+      '#weight' => 100,
+    ];
+    return $form;
   }
 
 }
