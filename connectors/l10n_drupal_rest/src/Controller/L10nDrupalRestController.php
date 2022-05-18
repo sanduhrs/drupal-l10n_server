83e0006383228dcb844c65f634d1c232e9a61b2d
sanduhrs <stefan@auditor.email>
2022-05-18 18:44:19 +0200

    Update Drupal core translation status page

--- connectors/l10n_drupal_rest/src/Controller/L10nDrupalRestController.php
+++ connectors/l10n_drupal_rest/src/Controller/L10nDrupalRestController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\l10n_drupal_rest\Controller;
 
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -31,8 +32,8 @@ class L10nDrupalRestController extends ControllerBase {
     uksort($releases, 'version_compare');
     $rid = array_reverse(array_keys($releases))[0];
 
-    // @todo l10n_drupal_rest.drupal_core.inc::l10n_drupal_rest_drupal_core_status
-    $num_source = 10666;
+    require_once __DIR__ . '/../../../../l10n_community/pages.inc';
+    [$num_source, $string_counts] = l10n_community_get_l10n_packager_string_count($project_id, $rid);
 
     $build['status'] = [
       '#type' => 'html_tag',
@@ -42,6 +43,45 @@ class L10nDrupalRestController extends ControllerBase {
           '@num' => number_format($num_source),
           '@release' => $releases[$rid],
         ]),
+    ];
+
+    $languages = \Drupal::languageManager()->getLanguages();
+
+    $table_rows = [];
+    foreach ($languages as $langcode => $language) {
+      // @todo Fix by porting l10n_packager module.
+      // $query = \Drupal::database()->select('l10n_server_translation')
+      //   ->condition('is_suggestion', 0)
+      //   ->condition('is_active', 1)
+      //   ->condition('language', $langcode);
+      // $user_count = $query->countQuery()->execute()->fetchObject();
+      $table_rows[] = [
+        [
+          'data' => new FormattableMarkup('<a href=":link">' . t('@language_name', ['@language_name' => $language->getName()]) . '</a>', [':link' => '/translate/languages/' . $langcode]),
+          // 'sortdata' => t('@language_name', ['@language_name' => $language->getName()]),
+        ],
+        [
+          'data' => ['#theme' => 'progress_bar', '#percent' => 35, '#message' => $this->t('@percent% translated (@num strings to go)', ['@percent' => 35, '@num' => 0])],
+          // 'sortdata' => ($num_source == 0 ? 0 : round(($string_counts[$langcode]['translations'] ?? 0) / $num_source * 100, 2)),
+        ],
+        [
+          'data' => $contributors ?? $this->t('n/a'),
+          // 'sortdata' => $contributors ?? $this->t('n/a'),
+        ],
+      ];
+    }
+
+    $header = [
+      ['data' => t('Language'), 'field' => 'language', 'class' => ['rowhead']],
+      ['data' => t('Drupal @release core progress', ['@release' => $releases[$rid]]), 'field' => 'progress'],
+      ['data' => t('Contributors'), 'field' => 'contributors'],
+    ];
+    $build['progress'] = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $table_rows,
+      '#empty' =>t('No files found'),
+      '#sticky' => TRUE,
     ];
     return $build;
   }
